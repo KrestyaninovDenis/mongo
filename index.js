9a1da883a7e256bedb0a1a6d9383dacbb394a0a6
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-18 16:12:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require ('express');
+const app = express();
+
+const cors = require ('cors');
+const bodyParser = require ('body-parser');
+
+const indexRouter = require('./routes/index');
+const bookRouter = require('./routes/book');
+const errorMiddleware = require('./middleware/error');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+app.set("view engine", "ejs");
+
+app.use('/', indexRouter);
+app.use('/book', bookRouter);
+app.use(errorMiddleware);
+/*
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Сервер стартовал, порт: ${PORT}`);
+});
+*/
+
+const PORT = process.env.PORT || 3000;
+async function start() {
+    try {
+        await 
+        mongoose.connect('mongodb://94.228.115.129:27017/books_database');
+        app.listen(PORT, () => {
+            console.log(`Сервер стартовал, порт: ${PORT}`);
+        })   
+    } 
+    catch (e) {
+        console.log(e);   
+}}
+start();
+
+const Todo = require('../../models/todo');
+const newTodo = new Todo({
+    title: 'title...',
+    desc: 'desc...',
+});
+try {
+    await 
+    newTodo.save();
+} 
+catch (e) {
+    console.error(e);
+}
