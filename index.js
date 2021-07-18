1c3daa01b3366eefec2a320aa67d14ac635378a8
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-18 19:02:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const mongoose = require('mongoose');
+
+const errorMiddleware = require('./middleware/error');
+
+const indexRouter = require('./routes/index');
+const bookRouter = require('./routes/book');
+const app = express();
+
+//const cors = require ('cors');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+//app.use(cors());
+app.set("view engine", "ejs");
+
+app.use('/', indexRouter);
+app.use('/book', bookRouter);
+app.use(errorMiddleware);
+/*
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Сервер стартовал, порт: ${PORT}`);
+});
+*/
+
+const PORT = process.env.PORT || 3000;
+const UserDB = process.env.DB_USERNAME || 'root';
+const PasswordDB = process.env.DB_PASSWORD || 'password';
+const NameDB = process.env.DB_NAME || 'todos_database'
+const HostDb = process.env.DB_HOST || 'mongodb://localhost:27017/todos_database'
+async function start() {
+    try {        
+        await mongoose.connect(HostDb, {
+            user: UserDB,
+            pass: PasswordDB,
+            dbName: NameDB,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        app.listen(PORT, () => {
+            console.log(`Старт сервера ${PORT}`);
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
---
+++ routes/book.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
+const Todo = require('../models/book')
+
+router.get('/', async (req, res) => {
+    const todo = await Todo.find();
+    res.render("book/index", {
+        title: "ToDo",
+        books: todo,
+    });
+});
+/*
+router.get('/create', (req, res) => {
+    res.render("book/create", {
+        title: "ToDo | create",
+        book: {},
+    });
+});
+
+router.post('/create', async (req, res) => {
+    const {title, desc} = req.body;
+
+    const newTodo = new Todo({
+        title, desc,
+    });
+
+    try {
+        await newTodo.save();
+        res.redirect('/todo');
+    } catch (e) {
+        console.error(e);
+    }
+});
+*/
+
+/*
+const {Book} = require('../models');
+const stor = {
+    books: [],
+}; 
+[1, 2, 3, 4].map(el => {
+    const newBook = new Book(`title ${el}`, `description ${el}`, `authors ${el}`, `favorite ${el}`, `fileCover ${el}`, `fileName ${el}`, `fileBook${el}`);
+    stor.books.push(newBook);
+});
+
+router.get('/', (req, res) => { //получаем массив всех книг
+    const {books} = stor;
+    res.render("book/index", {
+        title: "Список книг",
+        books: books,
+    });
+});
+
+router.get('/create', (req, res) => {
+    res.render("book/create", {
+        title: "Создание книги",
+        Book: {},
+    });
+});
+
+router.post('/create', (req, res) => {
+    const {books} = stor;
+    const {title, description, authors} = req.body;
+    const newBook = new Book(title, description, authors);
+    books.push(newBook);
+    res.redirect('/book')
+});
+
+router.get('/:id', (req, res) => {
+    const {books} = stor;
+    const {id} = req.params;
+    const idx = books.findIndex(el => el.id === id);
+    if (idx !== -1) {
+        res.render("book/view", {
+            title: "Просмотр книги",
+            book: books[idx],
+        });    
+    } else {
+        res.status(404).redirect('/404');
+    }
+});
+
+router.get('/update/:id', (req, res) => {
+    const {books} = stor;
+    const {id} = req.params;
+    const idx = books.findIndex(el => el.id === id);
+    if (idx !== -1) {
+        res.render("book/update", {
+            title: "Редактирование книги",
+            Book: books[idx],
+        });
+    } else {
+        res.status(404).redirect('/404');
+    }
+});
+
+router.post('/update/:id', (req, res) => {
+    const {books} = stor;
+    const {id} = req.params;
+    const {title, description, authors} = req.body;
+    const idx = books.findIndex(el => el.id === id);
+    if (idx !== -1) {
+        books[idx] = {
+            ...books[idx],
+            title,
+            description,
+            authors
+        };
+        res.redirect(`/book/${id}`);
+    } else {
+        res.status(404).redirect('/404');
+    }
+});
+
+router.post('/delete/:id', (req, res) => {
+    const {books} = stor;
+    const {id} = req.params;
+    const idx = books.findIndex(el => el.id === id);
+
+    if (idx !== -1) {
+        books.splice(idx, 1);
+        res.redirect(`/book`);
+    } else {
+        res.status(404).redirect('/404');
+    }
+});
+*/
+
+module.exports = router;
