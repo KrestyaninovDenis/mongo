1c1e39aa8b3b9ff36d267ac2ed5a099c0c84b54b
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-18 17:41:28 +0500

    Update book.js

--- routes/book.js
+++ routes/book.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 //const {Book} = require('../models');
 //const Todo = require('../models/todo');
-const Book = require('../models/book');
+const Book = require('../models');
 /*
 const stor = {
     books: [],
